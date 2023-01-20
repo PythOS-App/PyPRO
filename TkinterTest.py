b0b2b148f0cea9f852841330330a5327361030d5
Captain Awesome Jnr <90992828+captainawesomejnr@users.noreply.github.com>
2023-01-21 08:53:56 +1100

    Delete TkinterTest.py

--- TkinterTest.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-Main = tk.Tk()
-OsQuestion = tk.Label(text="Choose your operating system:")
-greeting.pack
-AppleIsOS = tk.button(text="MacOS or any other Apple software")
-WindowsIsOS = tk.button(text"Windows")
-OtherOS = tk.button(text = "Other operating system (eg. Linux)")
